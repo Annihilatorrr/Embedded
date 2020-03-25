265c3fc3a1ddc11e35f232621cdc491a08f87b74
ruslan_g <xsabitov@gmail.com>
2020-03-25 16:09:57 +0300

    Refactoring

--- AVR/AvrSolutions190320/LcdI2c/twi.cpp
+++ AVR/AvrSolutions190320/LcdI2c/twi.cpp
@@ -1,10 +1,6 @@
 ﻿#define F_CPU 8000000UL
 
 #include <avr/io.h>
-#include <avr/interrupt.h>
-#include <util/delay.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include "twi.h"
 
 void I2cTwix::init (void)
